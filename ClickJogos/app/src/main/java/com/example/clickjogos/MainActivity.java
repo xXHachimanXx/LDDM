dda57dcfe5cd5c8f37d75763b9e84b545c5290a1
Daniel Oliveira <daniel-santosoliveira753@gmail.com>
2019-09-03 16:56:34 -0300

    Última Vitoria do dia.

--- ClickJogos/app/src/main/java/com/example/clickjogos/MainActivity.java
+++ ClickJogos/app/src/main/java/com/example/clickjogos/MainActivity.java
@@ -2,18 +2,25 @@ package com.example.clickjogos;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,19 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         ImageView animais = findViewById(R.id.animaisId);
 
         ImageView adivinha = findViewById(R.id.jogoAdivinhaID);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         animais.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,12 +67,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         // Handle navigation view item clicks here.
         int id = menuItem.getItemId();
         if (id == R.id.AnimaisId) {
+            Log.d("Navigation", "onNavigationItemSelected: deu certo");
             startActivity(new Intent(MainActivity.this, SonsAnimaisActivity.class));
             return true;
         } else if (id == R.id.AdivinhaId) {
+            startActivity(new Intent(MainActivity.this, JogoAdvinha.class));
+            return true;
         }
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        drawerLayout.closeDrawer(GravityCompat.START);
 
         return true;
     }
