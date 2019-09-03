0286f588d0051dd7eb2260a33099f7c417f87eff
Daniel Oliveira <daniel-santosoliveira753@gmail.com>
2019-09-03 16:28:55 -0300

    Segunda Vitoria do dia.

--- ClickJogos/app/src/main/java/com/example/clickjogos/JogoAdvinha.java
+++ ClickJogos/app/src/main/java/com/example/clickjogos/JogoAdvinha.java
@@ -1,12 +1,17 @@
 package com.example.clickjogos;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NavUtils;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.MenuItem;
 
 import java.util.Random;
 
@@ -17,8 +22,25 @@ public class JogoAdvinha extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jogo_advinha);
 
-        Random random = new Random();
-        int numero = random.nextInt(100);
+        Button btnChutar = findViewById(R.id.btn_chutar);
+        final EditText input = findViewById(R.id.et_numero);
+
+        btnChutar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String chute = input.getText().toString().trim();
+
+                if (!TextUtils.isEmpty(chute)) {
+                    Random random = new Random();
+                    int numero = random.nextInt(100);
+
+                    if (Integer.parseInt(chute) == numero)
+                        Toast.makeText(getApplicationContext(), "Você acertou mizeravi", Toast.LENGTH_LONG).show();
+                    else
+                        Toast.makeText(getApplicationContext(), "Você errou :( \n Era " + numero, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     @Override
