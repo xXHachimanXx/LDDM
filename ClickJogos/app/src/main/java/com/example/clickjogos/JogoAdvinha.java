68edec606d090f59df465256a797d838e6ddf97d
xXHachimanXx <globaldep90@gmail.com>
2019-09-24 15:06:30 -0300

    Prox projeto.

--- ClickJogos/app/src/main/java/com/example/clickjogos/JogoAdvinha.java
+++ ClickJogos/app/src/main/java/com/example/clickjogos/JogoAdvinha.java
@@ -1,6 +1,7 @@
 package com.example.clickjogos;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.MenuItem;
@@ -15,15 +16,45 @@ import androidx.core.app.NavUtils;
 
 import java.util.Random;
 
-public class JogoAdvinha extends AppCompatActivity {
+public class JogoAdvinha extends AppCompatActivity
+{
+    private Button buttonSalvar;
+    private EditText chuteNumero;
+    private Button btnChutar;
+    public static final String Deveras = "Frustrante"; // ;-; DESISTO ;-;
+
+    private static final String PREFERENCE = "filePreference";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jogo_advinha);
-
-        Button btnChutar = findViewById(R.id.btn_chutar);
+        inicializarVariaveis();
         final EditText input = findViewById(R.id.et_numero);
+
+        buttonSalvar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                //0 - só pode ser lido por esta app
+                SharedPreferences sharedPreferences = getSharedPreferences(PREFERENCE, 0);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                //verifica se o texto é vazio
+                if (chuteNumero.getText().toString().equals(""))
+                {
+                    Toast.makeText(JogoAdvinha.this, "Digite o nome", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    editor.putString("nome", chuteNumero.getText().toString());
+                    editor.commit();
+
+                    Toast.makeText(JogoAdvinha.this, "Seu chute " + chuteNumero.getText().toString() + " foi guardado.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         btnChutar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,6 +72,25 @@ public class JogoAdvinha extends AppCompatActivity {
                 }
             }
         });
+
+        //recuperar os dados
+        SharedPreferences sharedPreferences = getSharedPreferences(PREFERENCE, 0);
+        if ( sharedPreferences.contains("nome") )
+        {
+            String userName = sharedPreferences.getString("nome", "Usuário não encontrado");
+            String numero = sharedPreferences.getString("Número", "Usuário não encontrado");
+            textViewNome.setText(textViewNome.getText() + userName);
+            textViewNumero.setText(textViewNumero.getText() + numero);
+        } else
+            Toast.makeText(getApplicationContext(), "Fudeu", Toast.LENGTH_LONG).show();
+    }
+
+
+
+    private void inicializarVariaveis()
+    {
+        this.btnChutar = findViewById(R.id.btn_chutar);
+        this.buttonSalvar = findViewById(R.id.idbuttonSalvar);
     }
 
     @Override
