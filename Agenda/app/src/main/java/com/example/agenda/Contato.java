7f48a63c39c46121eaeb16a79b61554f12016ba8
xXHachimanXx <globaldep90@gmail.com>
2019-10-23 17:29:03 -0200

    Sincronizando com Firebase e adicionando novo layout com mais inputs

--- Agenda/app/src/main/java/com/example/agenda/Contato.java
+++ Agenda/app/src/main/java/com/example/agenda/Contato.java
@@ -5,30 +5,48 @@ public class Contato
     private int id;
     private String nome;
     private String email;
+    private String endereco;
+    private String cep;
+
+    public Contato(){}
 
     public Contato (String nome, String email)
     {
         this.nome = nome;
         this.email = email;
     }
+    public Contato (String nome, String email, String endereco, String cep)
+    {
+        this.nome = nome;
+        this.email = email;
+        this.endereco = endereco;
+        this.cep = cep;
+    }
 
     @Override
     public String toString()
     {
-        return nome + "\n" + email + "\n" + id;
+        return nome + "\n" + email + "\n" + "Endereço: " + endereco;
     }
 
-    public void setId (int id) { this.id = id; }
+    public int getId() { return id; }
 
-    public int getId() { return this.id; }
+    public void setId(int id) { this.id = id; }
 
-    public String getNome()
-    {
-        return this.nome;
-    }
+    public String getNome() { return nome; }
 
-    public String getEmail()
-    {
-        return this.email;
-    }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
+
+    public String getEndereco() { return endereco; }
+
+    public void setEndereco(String endereco) { this.endereco = endereco; }
+
+    public String getCep() { return cep; }
+
+    public void setCep(String cep) { this.cep = cep; }
+
 }
