0c094acdb3fa36bb64ba015628ca8afd01a578de
xXHachimanXx <globaldep90@gmail.com>
2019-11-01 21:06:53 -0200

    Resolvendo conflitos e adicionando o projeto novamente

---
+++ WebServices/app/src/main/java/com/example/webservices/Endereco.java
@@ -0,0 +1,65 @@
+package com.example.webservices;
+
+import android.widget.EditText;
+
+public class Endereco
+{
+    private String Cep;
+    private String Logradouro;
+    private String Complemento;
+    private String Bairro;
+    private String Cidade;
+    private String UF;
+
+    public Endereco(){
+
+    }
+
+    public String getCep() {
+        return Cep;
+    }
+
+    public void setCep(String cep) {
+        Cep = cep;
+    }
+
+    public String getLogradouro() {
+        return Logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        Logradouro = logradouro;
+    }
+
+    public String getComplemento() {
+        return Complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        Complemento = complemento;
+    }
+
+    public String getBairro() {
+        return Bairro;
+    }
+
+    public void setBairro(String bairro) {
+        Bairro = bairro;
+    }
+
+    public String getCidade() {
+        return Cidade;
+    }
+
+    public void setCidade(String cidade) {
+        Cidade = cidade;
+    }
+
+    public String getUF() {
+        return UF;
+    }
+
+    public void setUF(String UF) {
+        this.UF = UF;
+    }
+}
---
+++ WebServices/app/src/main/java/com/example/webservices/WebServiceEndereco.java
@@ -0,0 +1,22 @@
+package com.example.webservices;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+
+
